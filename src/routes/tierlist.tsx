7d19dafb52462179260f3cf3008c01ae4d550191
Jonas Pöppelmann <jonas.poeppelmann@stf-gruppe.de>
2026-03-28 01:26:11 +0100

    Remove unused ConfidenceBar import from tierlist route

--- src/routes/tierlist.tsx
+++ src/routes/tierlist.tsx
@@ -1,6 +1,5 @@
 import { createFileRoute } from "@tanstack/react-router";
 import { useState } from "react";
-import { ConfidenceBar } from "@/components/ConfidenceBar";
 import { TierlistPreview } from "@/components/TierlistPreview";
 import { usePokemon } from "@/hooks/usePokemon";
 import { useTierlist } from "@/hooks/useTierlist";
